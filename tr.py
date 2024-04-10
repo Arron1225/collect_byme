85cd97da2b827aca433a2ad887689820d8ab3e31
Arron1225 <a0966821286@gmail.com>
2024-04-10 22:18:40 +0800

    change

--- tr.py
+++ tr.py
@@ -49,48 +49,21 @@ urls_tmp=[]
 browser = webdriver.Chrome()
 browser.get('https://tw.news.yahoo.com/search?p=股票&fr=uh3_news_web&fr2=p%3Anews%2Cm%3Asb&.tsrc=uh3_news_web')
 item=[]
-# last = browser.execute_script("return document.body.scrollHeight")  # Browser window height
-# i = 1
-# while True:
-#     # Scroll down
-#     browser.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-#     # i += 1
-#     # time.sleep(3)
-#     new = browser.execute_script("return document.body.scrollHeight")
-#     if new == last:
-#         break
-#     last =new
-    # Check if reaching the end of the page
-    # scroll_height = browser.execute_script("return document.body.scrollHeight;")
-    # if screen_height * i > scroll_height:
-    #     break
-# screen_height = browser.execute_script("return window.screen.height;")   # get the screen height of the web
-# i = 1
-last_height = browser.execute_script("return document.body.scrollHeight")
+
+scroll_pause_time = 2  # Pause between each scroll
+screen_height = browser.execute_script("return window.screen.height;")  # Browser window height
+i = 1
 while True:
-    # Scroll down to the bottom
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Scroll down
+    browser.execute_script(f"window.scrollTo(0, {screen_height * i});")
+    i += 1
+    time.sleep(scroll_pause_time)
 
-    # Wait for new page segment to load
-    time.sleep(5)  # Adjust the sleep duration based on your network speed
-
-    # Calculate new scroll height and compare with last scroll height
-    new_height = browser.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
+    # Check if reaching the end of the page
+    scroll_height = browser.execute_script("return document.body.scrollHeight;")
+    if screen_height * i > scroll_height:
         break
-    last_height = new_height
 
-# while True:
-#     # scroll one screen height each time
-#     browser.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
-#     i += 1
-#     time.sleep(5)
-#     # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
-#     scroll_height = browser.execute_script("return document.body.scrollHeight;")  
-#     # Break the loop when the height we need to scroll to is larger than the total scroll height
-#     if (screen_height) * i > scroll_height:
-#         break 
-# Fetch the data using BeautifulSoup after all data is loaded
 soup = BeautifulSoup(browser.page_source, "html.parser")
 # Process and save the data as needed
 head = soup.find_all("a",class_="Fw(b) Fz(20px) Lh(23px) Fz(17px)--sm1024 Lh(19px)--sm1024 mega-item-header-link Td(n) C(#0078ff):h C(#000) LineClamp(2,46px) LineClamp(2,38px)--sm1024 not-isInStreamVideoEnabled")
